956324dc099bdcdeb2b21ea4329e42e95d566004
Jan Malchert <25508038+JanMalch@users.noreply.github.com>
2020-03-16 19:22:36 +0100

    feat: add optional operator
    The operator can mark a resource as optional, so that when the server responds with the 404 status, the operator will safely return undefined instead. All other errors will be rethrown.

---
+++ projects/ngx-operators/src/lib/optional.spec.ts
@@ -0,0 +1,34 @@
+import {of, throwError} from 'rxjs'
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http'
+import {optional} from './optional';
+
+describe('optional', () => {
+  it('should return undefined if error status is 404', done => {
+    throwError(new HttpErrorResponse({status: 404})).pipe(
+      optional()
+    ).subscribe(
+      value => {
+        expect(value).toBeUndefined()
+        done()
+      }, fail)
+  })
+  it('should rethrow all other errors', done => {
+    throwError(new HttpErrorResponse({status: 500})).pipe(
+      optional()
+    ).subscribe(
+      fail, err => {
+        expect(err.status).toEqual(500)
+        done()
+      })
+  })
+  it('should pass on successful results', done => {
+    const response = new HttpResponse({status: 200, body: 'success'})
+    of(response).pipe(
+      optional()
+    ).subscribe(
+      value => {
+        expect(value?.body).toEqual('success')
+        done()
+      }, fail)
+  })
+})
