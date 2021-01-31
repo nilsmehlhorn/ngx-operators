c8893a51daf3e1c2e4a6686b0302858ac944d759
mehlhorn <mail@nils-mehlhorn.de>
2021-01-31 17:43:32 +0100

    refactor: restructure upload operator

--- projects/ngx-operators/src/lib/upload.ts
+++ projects/ngx-operators/src/lib/upload.ts
@@ -12,25 +12,26 @@ export function upload(): (
   source: Observable<HttpEvent<unknown>>
 ) => Observable<Upload> {
   const initialState: Upload = { state: "PENDING", progress: 0 };
-  return (source: Observable<HttpEvent<unknown>>) =>
+  const reduceState = (upload: Upload, event: HttpEvent<unknown>): Upload => {
+    if (isHttpProgressEvent(event)) {
+      return {
+        progress: event.total
+          ? Math.round((100 * event.loaded) / event.total)
+          : upload.progress,
+        state: "IN_PROGRESS",
+      };
+    }
+    if (isHttpResponse(event)) {
+      return {
+        progress: 100,
+        state: "DONE",
+      };
+    }
+    return upload;
+  };
+  return (source) =>
     source.pipe(
-      scan((download: Upload, event): Upload => {
-        if (isHttpProgressEvent(event)) {
-          return {
-            progress: event.total
-              ? Math.round((100 * event.loaded) / event.total)
-              : download.progress,
-            state: "IN_PROGRESS",
-          };
-        }
-        if (isHttpResponse(event)) {
-          return {
-            progress: 100,
-            state: "DONE",
-          };
-        }
-        return download;
-      }, initialState),
+      scan(reduceState, initialState),
       distinctUntilChanged(
         (a, b) => a.state === b.state && a.progress === b.progress
       )
