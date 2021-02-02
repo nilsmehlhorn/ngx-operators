ce21510e2aef88c990171dc6abfc3dc163cf4d24
mehlhorn <mail@nils-mehlhorn.de>
2021-02-02 11:29:41 +0100

    build: fix coverage reporting

--- projects/ngx-operators/karma.conf.js
+++ projects/ngx-operators/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require("path").join(__dirname, "../../coverage/ngx-operators"),
-      reports: ["html", "lcovonly", "text-summary"],
+      reports: ["lcovonly", "text-summary"],
       fixWebpackSourcePaths: true
     },
     customLaunchers: {
